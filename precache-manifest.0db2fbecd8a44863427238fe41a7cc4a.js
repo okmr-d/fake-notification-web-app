16c5b53be9345fa76031602cae9594f72b21a6d1
Daiki Okumura <daiki.okumura@gmail.com>
2018-11-11 16:51:39 +0900

    Updates

---
+++ precache-manifest.0db2fbecd8a44863427238fe41a7cc4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e570a7be64453c5bd40b47f410fe830b",
+    "url": "/fake-notification-web-app/static/media/background.e570a7be.jpg"
+  },
+  {
+    "revision": "1ab9280f8e0afb63a832",
+    "url": "/fake-notification-web-app/static/js/runtime~main.1ab9280f.js"
+  },
+  {
+    "revision": "4892e6e000aa3e4dc3d2",
+    "url": "/fake-notification-web-app/static/js/main.4892e6e0.chunk.js"
+  },
+  {
+    "revision": "b95e3f3e8bd7367c691f",
+    "url": "/fake-notification-web-app/static/js/1.b95e3f3e.chunk.js"
+  },
+  {
+    "revision": "4892e6e000aa3e4dc3d2",
+    "url": "/fake-notification-web-app/static/css/main.3015f57e.chunk.css"
+  },
+  {
+    "revision": "ac71f30e98a07abe6bf468f6872a3a30",
+    "url": "/fake-notification-web-app/index.html"
+  }
+];
